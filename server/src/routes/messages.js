d4852fcc00a5cb1f1056fba5ea0e2ed74fec9104
azzaky raffi <azzakramd27@gmail.com>
2026-08-17 19:40:10 +0700

    repair messages.js

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import pool from '../config/db.js';
 
-const router = express.router();
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
